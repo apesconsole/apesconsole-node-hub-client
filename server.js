4438434685fe59a0d7b75b530fb5e5a2eaf2677d
Ape's Console <apesconsole@gmail.com>
2017-05-09 13:52:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var logger = require("logging_component");
 var url = require("url");
 var mqtt = require('mqtt');
 
-var mqtt_url = url.parse('tcp://m13.cloudmqtt.com:16786');
+var mqtt_url = url.parse('tcp://localhost:0000');
 
 /* PIN SET UP */
 var rpio = require('rpio');
@@ -50,58 +50,74 @@ var path = __dirname + '/public/';
 app.use('/resources', express.static(path + 'resources'));
 app.use("/", router);
 
-var globalRoomData = [
-	{ title: 'Hall', id: 1, icon: 'hall', deviceList: [
-		{ title: 'Lamp', id: 'hall-light1' , status: false},
-		{ title: 'AC', id: 'random-1' , status: false},
-		{ title: 'Music', id: 'random-2' , status: false}
-	]},
-	{ title: 'Master Room', id: 2, icon: 'master', deviceList: [
-		{ title: 'Light', id: 'mstrm-light1' , status: false},
-		{ title: 'Music', id: 'random-3' , status: false},
-		{ title: 'Heater', id: 'random-4' , status: false}						
-	]},
-	{ title: 'Guest Room', id: 3, icon: 'guest', deviceList: [
-		{ title: 'Light', id: 'gstrm-light1' , status: false}						
-	]},
-	{ title: 'Garden', id: 4, icon: 'garden', deviceList: [
-		{ title: 'Soil', id: 'sensor-status' , status: false},
-		{ title: 'Sprinkler', id: 'water-pump' , status: false}
-	]}
-];
-
 
 var toggleDevice = function(deviceId){
 	switch(deviceId){
-		case 'sensor-status': logger.log('Sensor'); return false;
-		case    'water-pump': logger.log('Pump'); rpio.read(pump) == 1 ? rpio.write(pump, rpio.LOW) : rpio.write(pump, rpio.HIGH); return rpio.read(pump) == 1;
-		case   'hall-light1': logger.log('Lamp'); rpio.read(lamp) == 1 ? rpio.write(lamp, rpio.LOW) : rpio.write(lamp, rpio.HIGH); return rpio.read(lamp) == 1; 
-                case  'mstrm-light1': logger.log('led 1'); rpio.read(led1) == 1 ? rpio.write(led1, rpio.LOW) : rpio.write(led1, rpio.HIGH); return rpio.read(led1) == 1;
-                case  'gstrm-light1': logger.log('led 2'); rpio.read(led2) == 1 ? rpio.write(led2, rpio.LOW) : rpio.write(led2, rpio.HIGH); return rpio.read(led2) == 1;
-        }
+		case    'water-pump': 
+			logger.log('Pump'); 
+			if(rpio.read(pump) == 1)
+				rpio.write(pump, rpio.LOW);
+			else 
+				rpio.write(pump, rpio.HIGH); 
+			return rpio.read(pump) == 1;
+		case   'hall-light1': 
+			logger.log('Lamp'); 
+			if(rpio.read(lamp) == 1)
+				rpio.write(lamp, rpio.LOW); 
+			else 
+				rpio.write(lamp, rpio.HIGH);
+			return rpio.read(lamp) == 1; 
+        case  'mstrm-light1': 
+			logger.log('led 1'); 
+			if(rpio.read(led1) == 1)
+				rpio.write(led1, rpio.LOW);
+			else 
+				rpio.write(led1, rpio.HIGH); 
+			return rpio.read(led1) == 1;
+		case  'gstrm-light1': 
+			logger.log('led 2'); 
+			if(rpio.read(led2) == 1)
+				rpio.write(led2, rpio.LOW);
+			else 
+				rpio.write(led2, rpio.HIGH);
+			return (rpio.read(led2) == 1);
+    }
 	return false;
 }
 
 var respond = function(message){
-	var deviceState = JSON.parse( message.toString());
-	var exit = false;
-	for(var i=0; i<globalRoomData.length; i++){
-		if(globalRoomData[i].id == eval(deviceState.roomId)){
-			for(var j=0;j<globalRoomData[i].deviceList.length;j++){
-				if(globalRoomData[i].deviceList[j].id == deviceState.deviceId){
-                                        //Synch Call to GPIO operation
-					var currentState = toggleDevice(deviceState.deviceId);
-					logger.log('Device Id:' + deviceState.deviceId + ', current state -' + currentState);
-					globalRoomData[i].deviceList[j].status = currentState;
-                                        client.publish('T_APESCONSOLE_RD', '{"roomId": "' + deviceState.roomId + '", "deviceId": "' + deviceState.deviceId + '", "status": ' + currentState + '}');
-					exit = true; 
-					break;
-				}
-			}
-		}
-                if(exit) break;
-	}	
+	var triggerMessage = JSON.parse( message.toString());
+	var currentState = toggleDevice(triggerMessage.deviceId);
+	logger.log('Device Id:' + triggerMessage.deviceId + ', current state -' + currentState);
+	if(client != null){
+		//Send Device Feed Back to HUB
+		client.publish(
+			'T_APESCONSOLE_RD', 
+			'{"roomId": "' + deviceState.roomId + '", "deviceId": "' + deviceState.deviceId + '", "status": ' + currentState + '}'
+		);
+	}		
 }
+
+var previousRead = null; 
+var pollSensor = function(){
+	var reading = rpio.read(sensor);
+	logger.log('Sensor Reading: ' + reading);
+	if(previousRead != reading && client != null){
+		//Send Sensor Feed Back to HUB
+		client.publish(
+			'T_APESCONSOLE_RD', 
+			'{"roomId": "' + deviceState.roomId + '", "deviceId": "soil-sensor", "status": ' + reading + '}'
+		);
+	}
+}
+/*	
+	Soil Sensor Poller activated
+	Send Soil Status to HUB every minute
+*/
+setInterval(function(){
+	pollSensor();
+}, 60000);
+
 
 // subscribe to a topic
 client.on('connect', function() { // When connected
@@ -136,4 +152,4 @@ http.listen(process.env.PORT || 3002, function(){
 	logger.log('        Process Port :' + process.env.PORT);
 	logger.log('        Local Port   :' + 3002);
 	logger.log('##################################################');
-});	
+});
